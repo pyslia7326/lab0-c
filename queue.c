ffadf86a21e423cfd2c41169db7d0d31edfbcfb3
pyslia7326 <peiroaya7326@gmail.com>
2025-04-26 17:54:13 +0800

    Implement q_new and q_free
    q_new allocates and initializes a new list_head structure.
    q_free frees all elements in the list and the list_head structure.

--- queue.c
+++ queue.c
@@ -7,11 +7,26 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new_head = malloc(sizeof(struct list_head));
+    if (!new_head) {
+        return NULL;
+    }
+    INIT_LIST_HEAD(new_head);
+    return new_head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head) {
+        return;
+    }
+    element_t *entry, *safe;
+    list_for_each_entry_safe(entry, safe, head, list) {
+        q_release_element(entry);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
